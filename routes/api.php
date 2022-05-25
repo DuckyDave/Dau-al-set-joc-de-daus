9bd4b5236a587929bc65597a14dd57423e0f6b09
DuckyDave <duckydave.abanades@gmail.com>
2022-05-25 12:32:27 +0200

    Added log an user in route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Un usuari específic vol entrar (login)
+Route::post('login', [UserAuthController::class, 'login'])->name('user.login');
