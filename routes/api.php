68ac6b329b40738d06ea5a9cff61af3ad27c6eb8
DuckyDave <duckydave.abanades@gmail.com>
2022-05-25 12:09:55 +0200

    Added new user registation route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// una persona vol registar-se com a usuari per poder jugar
+Route::post('register', [UserAuthController::class, 'register'])->name('user.register');
