9f782e88b51ba4d7a870ceb950e2d2e2c8dec4a8
DuckyDave <duckydave.abanades@gmail.com>
2022-05-29 18:49:20 +0200

    Fixed a typo in definition of fake nicknames

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'nick_name' => $this->faker->optional(0.2.'anònim')->userName(),
+            'nick_name' => $this->faker->optional(0.2,'anònim')->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
